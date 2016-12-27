0ec7ae748d9ed78c5801dd59c7d9afaf6c6566cc
smallrustation <lpmaze@gmail.com>
2016-12-27 16:25:51 -0500

    Final version 4, add allowed hosts

---
+++ static/js/netchan-jquery.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#jquery-button').click(function (event) {
+        alert("JQuery button click");
+    })
+    $('#color-test').hover(function () {
+        $(this).css('color', 'red');
+    },
+    function () {
+        $(this).css('color', 'black')
+    })
+});
