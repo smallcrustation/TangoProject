0c1e987c8620a22a643efb52b27d4c6c7be77019
smallrustation <lpmaze@gmail.com>
2016-12-27 17:37:01 -0500

    Final version 6, fucking static files root added

--- TangoProject/settings.py
+++ TangoProject/settings.py
@@ -17,6 +17,7 @@ import posixpath
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates') ### base_dir finds the sys dir for any1, join them
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 ### Quick-start development settings - unsuitable for production
