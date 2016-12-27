6fc284b56e3ed46d168701e5ce1c63ebda0ea8b7
smallrustation <lpmaze@gmail.com>
2016-12-27 14:07:16 -0500

    Final 3 edited pupulate.py

--- populate_netchan.py
+++ populate_netchan.py
@@ -13,39 +13,39 @@ def populate():
     python_pages = [
         {"title": "Official Python Tutorial",
         "url": "http://docs.python.org/2/tutorial/",
-        "views": 20},
+        "views": 0},
         {"title": "How to Think like a Computer Scientist",
         "url": "http://www.greenteapress.com/thinkpython/",
-        'views': 96},
+        'views': 0},
         {"title":"Learn Python in 10 Minutes",
         "url":"http://www.korokithakis.net/tutorials/python/",
-        'views': 5}
+        'views': 0}
          ]
     
     django_pages = [
         {"title":"Official Django Tutorial",
         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
-        'views': 8},
+        'views': 0},
         {"title":"Django Rocks",
         "url":"http://www.djangorocks.com/",
-        'views': 3},
+        'views': 0},
         {"title":"How to Tango with Django",
         "url":"http://www.tangowithdjango.com/",
-        'views': 6} 
+        'views': 0} 
         ]
 
     other_pages = [
         {"title":"Bottle",
         "url":"http://bottlepy.org/docs/dev/",
-        'views': 78},
+        'views': 0},
         {"title":"Flask",
         "url":"http://flask.pocoo.org",
-        'views': 54} 
+        'views': 0} 
         ]
 
-    categories = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-                  "Django": {"pages": django_pages, "views": 64, "likes": 32},
-                  "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}  }
+    categories = {"Python": {"pages": python_pages, "views": 0, "likes": 0},
+                  "Django": {"pages": django_pages, "views": 0, "likes": 0},
+                  "Other Frameworks": {"pages": other_pages, "views": 0, "likes": 0}  }
 
 
 
